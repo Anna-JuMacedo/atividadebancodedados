0c807590c9aa8470c91c530ad2712838ed5526be
Anna-JuMacedo <annajuliamacedo16@gmail.com>
2025-07-24 08:37:09 -0300

    funcionando

--- valida.js
+++ valida.js
@@ -1,42 +1,26 @@
-function validaSenha(){
-    let senha = document.getElementById("senha_usuario").value;
- 
-    if (senha.length >= 8) {
-        document.getElementById("val1").style.color = "blue";
-    }else{
-        document.getElementById("val1").style.color = "gray";
-    }
+function validaSenha() {
+   const senha = document.getElementById("senha_usuario").value;
 
- const regex_num = /[0-9]/;
+   const criterios = {
+       val1: senha.length >= 8,
+       val2: /[0-9]/.test(senha),
+       val3: /[A-Z]/.test(senha),
+       val4: /[a-z]/.test(senha),
+       val5: /[!@#$%^&*]/.test(senha)
+   };
 
- if(regex_num.test(senha)){
-    document.getElementById("val2").style.color = "blue";
- }else{
-    document.getElementById("val2").style.color = "gray";
- }
- 
- const regex_mai= /[A-Z]/;
-
- if(regex_num.test(senha)){
-    document.getElementById("val3").style.color = "blue";
- }else{
-    document.getElementById("val3").style.color = "gray";
- }
- const regex_min= /[a-z]/;
-
- if(regex_num.test(senha)){
-    document.getElementById("val4").style.color = "blue";
- }else{
-    document.getElementById("val4").style.color = "gray";
- }
-
- const regex_car= /[!@#$%^&*]/;
-
- if(regex_num.test(senha)){
-    document.getElementById("val5").style.color = "blue";
- }else{
-    document.getElementById("val5").style.color = "gray";
- }
-
+   for (let id in criterios) {
+       const item = document.getElementById(id);
+       if (criterios[id]) {
+           item.classList.remove("invalid");
+           item.classList.add("valid");
+       } else {
+           item.classList.remove("valid");
+           item.classList.add("invalid");
+       }
+   }
 }
- 
+
+document.addEventListener("DOMContentLoaded", () => {
+   document.getElementById("senha_usuario").addEventListener("input", validaSenha);
+});
